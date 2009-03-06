1a8414a08303a3eec73f6e1cc073adff2f3d8b1d
acleone <acleone@ec14e79c-f1a9-11dd-99c0-75d572ba1ddd>
2009-03-06 01:38:25 +0000

    Fixed missing pin definitions for PB5, etc for the ATmega328p, and changed the default NUM_TLCS to 1 in tlc_config.h (it was 3 before - I forgot to change it back after testing the library)
    git-svn-id: http://tlc5940arduino.googlecode.com/svn/trunk@10 ec14e79c-f1a9-11dd-99c0-75d572ba1ddd

--- Tlc5940/pinouts/chip_includes.h
+++ Tlc5940/pinouts/chip_includes.h
@@ -22,6 +22,39 @@
 /** \file
     Includes the chip-specfic defaults and pin definitions. */
 
+#include <avr/io.h>
+
+#ifndef PB0
+#define PB0     PORTB0
+#define PB1     PORTB1
+#define PB2     PORTB2
+#define PB3     PORTB3
+#define PB4     PORTB4
+#define PB5     PORTB5
+#define PB6     PORTB6
+#define PB7     PORTB7
+#endif
+#ifndef PC0
+#define PC0     PORTC0
+#define PC1     PORTC1
+#define PC2     PORTC2
+#define PC3     PORTC3
+#define PC4     PORTC4
+#define PC5     PORTC5
+#define PC6     PORTC6
+#define PC7     PORTC7
+#endif
+#ifndef PD0
+#define PD0     PORTD0
+#define PD1     PORTD1
+#define PD2     PORTD2
+#define PD3     PORTD3
+#define PD4     PORTD4
+#define PD5     PORTD5
+#define PD6     PORTD6
+#define PD7     PORTD7
+#endif
+
 /* Chip Specific Pinouts */
 #if defined (__AVR_ATmega168__)  \
  || defined (__AVR_ATmega168P__) \
